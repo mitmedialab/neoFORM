98a836256400aa8e0a94bdd5eb9a9e7e6ac2f6fb
neoform-client <neoform-client@media.mit.edu>
2022-08-15 16:16:16 -0400

    about to put in kinect code

---
+++ src/ExternalDeviceManagers/KinectManager.cpp
@@ -0,0 +1,8 @@
+//
+//  KinectManager.cpp
+//  neoForm
+//
+//  Created by admin on 8/15/22.
+//
+
+#include "KinectManager.hpp"
---
+++ src/ExternalDeviceManagers/KinectManager.hpp
@@ -0,0 +1,13 @@
+//
+//  KinectManager.hpp
+//  neoForm
+//
+//  Created by admin on 8/15/22.
+//
+
+#ifndef KinectManager_hpp
+#define KinectManager_hpp
+
+#include <stdio.h>
+
+#endif /* KinectManager_hpp */
