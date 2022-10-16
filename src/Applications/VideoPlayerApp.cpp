dd39eb53b8ef181a69bdbdc9dafc6064275a8547
Jonathan Williams <tamlin@mac.com>
2022-10-16 16:42:53 -0400

    :pencil: Doc comments

--- src/Applications/VideoPlayerApp.cpp
+++ src/Applications/VideoPlayerApp.cpp
@@ -33,10 +33,12 @@ void VideoPlayerApp::updateHeights() {
 
         for (int y = 0; y < SHAPE_DISPLAY_SIZE_Y; y++) {
             
+            // This takes the 2 dimensional coordinates and turns them into a one dimensional index for the unwrapped array.
             int unwrappedIndex = heightsForShapeDisplay.getPixelIndex(x, y);
             
             char foo = plz[m_videoToTransformIndicies[unwrappedIndex]];
             
+            // This takes the 1 dimensional index for the pin, and grabs the corresponding index from the uncorrected video pixel array.
             heightsForShapeDisplay[unwrappedIndex] = plz[m_videoToTransformIndicies[unwrappedIndex]];
         }
     }
@@ -53,6 +55,17 @@ string VideoPlayerApp::appInstructionsText() {
     return instructions;
 }
 
+
+// Given an x coordinate from the video (102 pixels wide),
+// We chop in into 6 segments
+// Segments that are used for actuation are 2, 4, 6
+// Other segments are not used by TRANSFORM
+// so 102 divided into 6 segments, means each segment is 17 pixels wide
+// + 5 pixels on each border
+// The returned block number is the number of the block where the
+// video pixel input to the function ends up.
+// blocks 1,3,5, useless
+// blocks 2,4,6 used in transform
 int VideoPlayerApp::calculateBlockNumber(int x_pixel_coord){
     //note: int divison returns a truncated result that looks like a floor:
     // 1/2 would return 0, 4/2.5 would return 1
@@ -61,10 +74,18 @@ int VideoPlayerApp::calculateBlockNumber(int x_pixel_coord){
     return blockNum;
 }
 
+// Once block has been established, the
+// x coordinate of the video pixel, and the block number are input to this function.
+// If its a useless block, (1, 3, or 5), made 0, will never be shown
+// If it is in a useful block (2,4,6), x_pixel coordinate will be translated into
+// x coordinate on the TRANSFORM pin.
 int VideoPlayerApp::calculateWithinBlockX(int blockNumber, int x_pixel_coord){
     int blockDead = blockNumber%2;
     
-    //find actual location on TRANSFORM grid from video
+    // Find actual location on TRANSFORM grid from video
+    // NOTE TRANSFORM display is 48 pixels wide.
+    // NOTE video pixels are 102 wide with 5 pixel buffer on left side. (hence 107 below)
+    // right side also has a 5 pixel buffer, but doesn't matter for our caculation
     int TRANSFORM_x = (int)((48.0/107.0)*(float)(x_pixel_coord+5));
     
     if (blockDead){
@@ -75,11 +96,16 @@ int VideoPlayerApp::calculateWithinBlockX(int blockNumber, int x_pixel_coord){
     return TRANSFORM_x;
 }
 
+// Runs only once on initialization.
+// Fills the m_videoToTransformIndicies array with a map between flattened TRANSFORM pin numbers
+// and flattened video pixels.
+// m_videoToTransformIndicies(flattened TRANSFORM pixel index) = (flattened video pixel index)
+// To assign TRANSFORM pixel video pixels values in the update, do something like
+// (pseudocode) ==> TRANSFORM_Pin_Height(pin1) = m_video_toTransformIndicies(pin1);
 void VideoPlayerApp::setupTransformedPixelMap(){
     int counter = 0;
     
-    //go through all
- 
+    // Iterate over all pixels in the video frame.
     for (int y = 0; y < 24; y++) {
         for (int x = 0; x < 102; x++) {
             int blockAliveXCoord = calculateWithinBlockX(calculateBlockNumber(x),x);
