25d7aaf2ae643fc940bcd9c523d5f99d036a026c
jdub <jdub@media.mit.edu>
2023-02-05 15:49:14 -0500

    move KinectManager and add Kinect Tracker

---
+++ src/KinectManager/KinectTracker.cpp
@@ -0,0 +1,8 @@
+//
+//  KinectTracker.cpp
+//  neoForm
+//
+//  Created by admin on 2/5/23.
+//
+
+#include "KinectTracker.hpp"
---
+++ src/KinectManager/KinectTracker.hpp
@@ -0,0 +1,13 @@
+//
+//  KinectTracker.hpp
+//  neoForm
+//
+//  Created by admin on 2/5/23.
+//
+
+#ifndef KinectTracker_hpp
+#define KinectTracker_hpp
+
+#include <stdio.h>
+
+#endif /* KinectTracker_hpp */
