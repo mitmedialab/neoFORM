b68d6a909bb0cf7da772683219787d7de3731e2c
jdub <jdub@media.mit.edu>
2023-07-16 14:14:50 -0400

    temporary cropped image area
    for actual surface

--- src/Applications/DebuggingApps/KinectDebugApp.cpp
+++ src/Applications/DebuggingApps/KinectDebugApp.cpp
@@ -27,10 +27,22 @@ void KinectDebugApp::update(float dt) {
 // Draw the depth data into the app preview window.
 void KinectDebugApp::drawGraphicsForShapeDisplay(int x, int y, int width, int height){
     cout << "they see us rollin, they hatin";
-    ofImage(m_kinectManager->depthPixels).draw(2, 2, m_kinectManager->getImageWidth(), m_kinectManager->getImageHeight());
+    //ofImage(m_kinectManager->depthPixels).draw(2, 2, m_kinectManager->getImageWidth(), m_kinectManager->getImageHeight());
     
     m_kinectManager->drawContours();
     
+    // Get the raw depth data cropped to the dimensions of the surface mask.
+    
+    
+    cout << m_kinectManager->depthPixels.getWidth() << endl;
+    ofPixels bob;
+    
+    m_kinectManager->depthPixels.cropTo(bob, m_kinectManager->m_mask.getX(), m_kinectManager->m_mask.getY(), m_kinectManager->m_mask.width, m_kinectManager->m_mask.height);
+    
+    cout << bob.getWidth() << endl;
+    
+    ofImage(bob).draw(2, 2, m_kinectManager->getImageWidth(), m_kinectManager->getImageHeight());
+
 }
 
 void KinectDebugApp::keyPressed(int key){
