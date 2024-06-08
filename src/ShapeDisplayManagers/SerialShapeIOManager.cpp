914182312f5ff2d4d4917c40e3790bcf87001593
Jonathan Williams <tamlin@mac.com>
2024-06-08 16:42:58 -0400

    remove unnecessary

--- src/ShapeDisplayManagers/SerialShapeIOManager.cpp
+++ src/ShapeDisplayManagers/SerialShapeIOManager.cpp
@@ -45,9 +45,6 @@ SerialShapeIOManager::SerialShapeIOManager(KinectManager* kinectRef) {
     // stuck pin safety toggling can only be implemented if we have height data
     // from the shape display telling us whether pins are stuck
     enableStuckPinSafetyToggle = enableStuckPinSafetyToggle && heightsFromShapeDisplayAvailable;
-    
-    // Size the pin arrays correctly based on the hardware specific dimension, and initialize them with zero values
-    heightsForShapeDisplay.resize(SHAPE_DISPLAY_SIZE_X, std::vector<unsigned char>(SHAPE_DISPLAY_SIZE_Y, 0));
 
     // initialize per-pin data arrays
     for (int x = 0; x < SHAPE_DISPLAY_SIZE_X; x++) {
