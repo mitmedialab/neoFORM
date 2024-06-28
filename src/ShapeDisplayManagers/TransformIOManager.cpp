a3e028cf8bcb4f6e751553c548402f53b7cbbe5b
TMG <tangiblemedia@icloud.com>
2024-06-28 16:34:42 -0400

    update board config

--- src/ShapeDisplayManagers/TransformIOManager.cpp
+++ src/ShapeDisplayManagers/TransformIOManager.cpp
@@ -21,63 +21,60 @@ TransformIOManager::TransformIOManager(KinectManager* kinectRef) {
 
 // setup transFORM-specific board configuration
 void TransformIOManager::configureBoards() {
-    // set up coordinates for
-    for (int i = 0; i < 96; i++) {  //NUM_ARDUINOS
-        // determine which serial connection each board is on:
-        // every 3rd and 4th board is on the second
-        if (i < 36) { //64
-            pinBoards[i].serialConnection = 0; //((i / 2) % 2 == 0) ? 0 : 1
-        } else if (i < 60) { //128
-            pinBoards[i].serialConnection = 1; //((i / 2) % 2 == 0) ? 2 : 3
-        } else {
-            pinBoards[i].serialConnection = 2; //((i / 2) % 2 == 0) ? 4 : 5
-        }
-        
-        // every 5th to 8th board is mounted upside down, so invert the height
-        if (i % 12 == 5 || i % 12 == 6 || i % 12 == 7 || i % 12 == 8) {
-            pinBoards[i].invertHeight = 1;
-        } else {
-            pinBoards[i].invertHeight = 0;
-        }
-        
-            
-        //pinBoards[i].invertHeight = ((i / 4) % 2 == 0) ? false : true;
-        
-        for (int j = 0; j < NUM_PINS_ARDUINO; j++) {
-            int currentRow = (int)(i / 4);
-            int currentColumn = j + (i % 4 * 6);
-            pinBoards[i].heights[j] = 0;
-            pinBoards[i].pinCoordinates[j][0] = currentRow;
-            pinBoards[i].pinCoordinates[j][1] = currentColumn;
-        }
-        
-        if ((i / 2) % 2 == 0) {
-            int pinCoordinateRows[NUM_PINS_ARDUINO];
-            
-            // invert pin order if the boards are mounted rotated
-            for (int count = 0; count < NUM_PINS_ARDUINO; count++) {
-                pinCoordinateRows[NUM_PINS_ARDUINO - count - 1] = pinBoards[i].pinCoordinates[count][1];
-            }
-            for (int count = 0; count < NUM_PINS_ARDUINO; count++) {
-                pinBoards[i].pinCoordinates[count][1] = pinCoordinateRows[count];
-            }
-            
-            // also invert the pin height again if they are:
-            pinBoards[i].invertHeight = !pinBoards[i].invertHeight;
-        }
-        
-        // last, orient the x-y coordinate axes to the desired external axes
-        for (int j = 0; j < NUM_PINS_ARDUINO; j++) {
-            unsigned char j0 = pinBoards[i].pinCoordinates[j][0];
-            unsigned char j1 = pinBoards[i].pinCoordinates[j][1];
-            pinBoards[i].pinCoordinates[j][0] = SHAPE_DISPLAY_SIZE_X - 1 - j0;
-            pinBoards[i].pinCoordinates[j][1] = SHAPE_DISPLAY_SIZE_Y - 1 - j1;
-        }
+  // set up coordinates for
+  for (int i = 0; i < 96; i++) { //NUM_ARDUINOS
+    // determine which serial connection each board is on:
+    // every 3rd and 4th board is on the second
+    if (i < 36) { //64
+      pinBoards[i].serialConnection = 0; //((i / 2) % 2 == 0) ? 0 : 1
+    } else if (i < 60) { //128
+      pinBoards[i].serialConnection = 1; //((i / 2) % 2 == 0) ? 2 : 3
+    } else {
+      pinBoards[i].serialConnection = 2; //((i / 2) % 2 == 0) ? 4 : 5
     }
-
-    // flag configuration as complete
-    boardsAreConfigured = true;
+    // every 5th to 8th board is mounted upside down, so invert the height
+    if (i % 12 == 4 || i % 12 == 5 || i % 12 == 6 || i % 12 == 7) {
+      //printf(“%d\n”, i % 12 == 8 || i % 12 == 9 || i % 12 == 10 || i % 12 == 11);
+      pinBoards[i].invertHeight = true;
+    } else{
+      printf("%d\n", i % 12 == 8 || i % 12 == 9 || i % 12 == 10 || i % 12 == 11);
+      pinBoards[i].invertHeight = false;
+    }
+    //pinBoards[i].invertHeight = ((i / 4) % 2 == 0) ? false : true;
+    for (int j = 0; j < NUM_PINS_ARDUINO; j++) {
+      int currentRow = (int)(i / 4);
+      int currentColumn = 5 - j + (i % 4 * 6);
+      pinBoards[i].heights[j] = 0;
+      pinBoards[i].pinCoordinates[j][0] = currentRow;
+      pinBoards[i].pinCoordinates[j][1] = currentColumn;
+    }
+//    if ((i / 2) % 2 == 0) { //
+//      int pinCoordinateRows[NUM_PINS_ARDUINO];
+//
+//       //invert pin order if the boards are mounted rotated
+//      for (int count = 0; count < NUM_PINS_ARDUINO; count++) {
+//        pinCoordinateRows[NUM_PINS_ARDUINO - count - 1] = pinBoards[i].pinCoordinates[count][1];
+//      }
+//      for (int count = 0; count < NUM_PINS_ARDUINO; count++) {
+//        pinBoards[i].pinCoordinates[count][1] = pinCoordinateRows[count];
+//      }
+//
+//      // also invert the pin height again if they are:
+//      pinBoards[i].invertHeight = !pinBoards[i].invertHeight;
+//    }
+    // last, orient the x-y coordinate axes to the desired external axes
+    for (int j = 0; j < NUM_PINS_ARDUINO; j++) {
+      unsigned char j0 = pinBoards[i].pinCoordinates[j][0];
+      unsigned char j1 = pinBoards[i].pinCoordinates[j][1];
+      pinBoards[i].pinCoordinates[j][0] = SHAPE_DISPLAY_SIZE_X - 1 - j0;
+      pinBoards[i].pinCoordinates[j][1] = SHAPE_DISPLAY_SIZE_Y - 1 - j1;
+    }
+  }
+  //printBoardConfiguration();
+  // flag configuration as complete
+  boardsAreConfigured = true;
 }
+
 
 ofPixels TransformIOManager::getKinectStream(){
     m_kinectManagerRef->update();
