19c8affedfbb05a43c21941b4027461aff438aaa
charlcoal <charley.reischer@gmail.com>
2024-10-20 16:03:26 -0400

    commented out unused code

--- src/Utils/utils.cpp
+++ src/Utils/utils.cpp
@@ -11,39 +11,39 @@ double elapsedTimeInSeconds() {
     return clock() / (double) CLOCKS_PER_SEC;
 }
 
-ofImage applyFunctionToPixels(const ofImage& first, const ofImage& second, pixelFunction fn) {
-    ofImage out = ofImage(first);
-
-    ofPixels& outPixels = out.getPixels();
-    const ofPixels& secondPixels = second.getPixels();
-
-    // weighted average of pixel
-    for (int i = 0; i < outPixels.size(); i++) {
-        outPixels[i] = fn(outPixels[i], secondPixels[i]);
-    }
-    
-    out.update();
-    return out;
-
-}
-
-ofImage absoluteDifference(const ofImage& first, const ofImage& second) {
-    return applyFunctionToPixels(first, second, [](unsigned char a, unsigned char b){return (unsigned char)abs(a - int(b));});
-}
-
-ofImage weightedAverage(const ofImage& first, const ofImage& second, double weight) {
-    ofImage out = ofImage(first);
-    weight = std::clamp(weight, 0.0, 1.0);
-
-    ofPixels& outPixels = out.getPixels();
-    const ofPixels& secondPixels = second.getPixels();
-
-    // weighted average of pixel
-    for (int i = 0; i < outPixels.size(); i++) {
-        outPixels[i] = (1.0 - weight) * outPixels[i] + weight * secondPixels[i];
-    }
-    
-    out.update();
-    return out;
-}
+//ofImage applyFunctionToPixels(const ofImage& first, const ofImage& second, pixelFunction fn) {
+//    ofImage out = ofImage(first);
+//
+//    ofPixels& outPixels = out.getPixels();
+//    const ofPixels& secondPixels = second.getPixels();
+//
+//    // weighted average of pixel
+//    for (int i = 0; i < outPixels.size(); i++) {
+//        outPixels[i] = fn(outPixels[i], secondPixels[i]);
+//    }
+//    
+//    out.update();
+//    return out;
+//
+//}
+//
+//ofImage absoluteDifference(const ofImage& first, const ofImage& second) {
+//    return applyFunctionToPixels(first, second, [](unsigned char a, unsigned char b){return (unsigned char)abs(a - int(b));});
+//}
+//
+//ofImage weightedAverage(const ofImage& first, const ofImage& second, double weight) {
+//    ofImage out = ofImage(first);
+//    weight = std::clamp(weight, 0.0, 1.0);
+//
+//    ofPixels& outPixels = out.getPixels();
+//    const ofPixels& secondPixels = second.getPixels();
+//
+//    // weighted average of pixel
+//    for (int i = 0; i < outPixels.size(); i++) {
+//        outPixels[i] = (1.0 - weight) * outPixels[i] + weight * secondPixels[i];
+//    }
+//    
+//    out.update();
+//    return out;
+//}
 
--- src/Utils/utils.hpp
+++ src/Utils/utils.hpp
@@ -15,11 +15,11 @@
 double elapsedTimeInSeconds();
 
 typedef unsigned char (*pixelFunction) (unsigned char, unsigned char);
-ofImage applyFunctionToPixels(const ofImage& first, const ofImage& second, pixelFunction function);
-// weight * second + (1 - weight) * first ----- with [0, 1] bounds on weight
-ofImage weightedAverage(const ofImage& first, const ofImage& second, double weight);
-// the absolute value of the difference between each pixel
-ofImage absoluteDifference(ofImage first, ofImage second);
+//ofImage applyFunctionToPixels(const ofImage& first, const ofImage& second, pixelFunction function);
+//// weight * second + (1 - weight) * first ----- with [0, 1] bounds on weight
+//ofImage weightedAverage(const ofImage& first, const ofImage& second, double weight);
+//// the absolute value of the difference between each pixel
+//ofImage absoluteDifference(ofImage first, ofImage second);
 
 #endif /* utils_hpp */
 
