da88c0b8a8b04428fb5918248b21a3c9abe15d16
charlcoal <charley.reischer@gmail.com>
2024-12-15 16:39:29 -0500

    added boats

--- src/Applications/AmbientWave.hpp
+++ src/Applications/AmbientWave.hpp
@@ -8,19 +8,29 @@
 #ifndef AmbientWave_hpp
 #define AmbientWave_hpp
 
+struct Boat {
+	double x;
+	double y;
+};
+
 #include "Application.hpp"
 #include "SerialShapeIOManager.hpp"
 class AmbientWave: public Application {
 public:
-	AmbientWave(SerialShapeIOManager* theCustomShapeDisplayManager) : Application(theCustomShapeDisplayManager) {}
+	AmbientWave(SerialShapeIOManager* theCustomShapeDisplayManager);
 
 	std::string getName() {return "Ambient Wave";}
     void update(float dt);
+
 protected:
 	double wave(double x, double y, double k_x, double k_y);
+	std::pair<double, double> gradientOfWave(double x, double y, double k_x, double k_y, double phase = 0);
+
 protected:
 	const double timescale = 5.0;
 	double currentTime = 0.0;
+
+	std::array<Boat, 5> boats;
 };
 
 
