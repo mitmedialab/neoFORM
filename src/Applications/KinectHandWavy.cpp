0732d0c85f8005a58db24366be0d1d75f9ebfe53
Jonathan Williams <tamlin@mac.com>
2024-12-27 21:52:18 -0500

    initial kinect hand wavy
    seems to work with inFORM mode, but not so well with TRANSFORM

--- src/Applications/KinectHandWavy.cpp
+++ src/Applications/KinectHandWavy.cpp
@@ -11,11 +11,11 @@
 #include "ofxXmlSettings.h"
 #include <opencv2/imgproc.hpp>
 
-KinectHandWavy::KinectHandWavy(KinectManager* kinectManager) {
+KinectHandWavy::KinectHandWavy(KinectManagerSimple* kinectManager) {
     m_kinectManager = kinectManager;
 }
 
-KinectHandWavy::KinectHandWavy(SerialShapeIOManager *theSerialShapeIOManager, KinectManager *theKinectManager)
+KinectHandWavy::KinectHandWavy(SerialShapeIOManager *theSerialShapeIOManager, KinectManagerSimple *theKinectManager)
 : Application(theSerialShapeIOManager) {
     
     m_kinectManager = theKinectManager;
@@ -28,10 +28,7 @@ void KinectHandWavy::setup() {
 }
 
 void KinectHandWavy::update(float dt) {
-    // prevents running without kinectManager
-    if (!m_kinectManager->isConnected()) return;
-
-    cout << "I'm updating!!!!!!";
+    //cout << "I'm updating!!!!!!";
     
     m_kinectManager->update();
     updateHeights();
@@ -39,19 +36,18 @@ void KinectHandWavy::update(float dt) {
 }
 // This is responsible for drawing the on screen preview of the app's behavior.
 void KinectHandWavy::drawGraphicsForShapeDisplay(int x, int y, int width, int height) {
-    // prevents running without kinectManager
-    if (!m_kinectManager->isConnected()) return;
-
     //*** Draw the color pixels for reference.
-    m_kinectManager->colorImg.draw(2, 2, m_kinectManager->getImageWidth(), m_kinectManager->getImageHeight());
+    ofImage colorImg = m_kinectManager->getColorPixels();
+    colorImg.draw(2, 2, colorImg.getWidth(), colorImg.getHeight());
     
     //*** Overlay the depth image on top of the color image.
     // Set the color to white with 50% opacity
     ofSetColor(255, 255, 255, 127);
 
     // Draw the depth image
-    m_kinectManager->depthImg.draw(2, 2, m_kinectManager->getImageWidth(), m_kinectManager->getImageHeight());
-
+    ofShortImage depthImg = m_kinectManager->getDepthPixels();
+    depthImg.draw(2, 2, depthImg.getWidth(), depthImg.getHeight());
+    
     // Reset the color to fully opaque white
     ofSetColor(255, 255, 255, 255);
 
@@ -59,8 +55,8 @@ void KinectHandWavy::drawGraphicsForShapeDisplay(int x, int y, int width, int he
     drawPreviewMaskRectangle();
 
     //*** Preview shape display pixels
-    ofxCvGrayscaleImage blurredDepthImg = getBlurredDepthImg();
-    blurredDepthImg.draw(2, 400, m_kinectManager->getImageWidth(), m_kinectManager->getImageHeight());
+    m_kinectManager->crop(depthImg.getPixels());
+    depthImg.draw(2, 400, depthImg.getWidth(), depthImg.getHeight());
 
     //*** Contours are disabled, but maybe they will be useful in the future.
     //m_kinectManager->drawContours();
@@ -73,9 +69,6 @@ void KinectHandWavy::drawGraphicsForShapeDisplay(int x, int y, int width, int he
 
 // Draw a rectangle around the shape display pixels based on the mask info from settings.xml
 void KinectHandWavy::drawPreviewMaskRectangle() {
-    // prevents running without kinectManager
-    if (!m_kinectManager->isConnected()) return;
-
     // Set the drawing parameters
     ofSetColor(0, 0, 255);
     ofNoFill();
@@ -83,10 +76,10 @@ void KinectHandWavy::drawPreviewMaskRectangle() {
     
     // Draw the rectangle with the dimensions of the mask.
     ofDrawRectangle(
-                    m_kinectManager->m_mask.getX(),
-                    m_kinectManager->m_mask.getY(),
-                    m_kinectManager->m_mask.getWidth(),
-                    m_kinectManager->m_mask.getHeight()
+                    m_kinectManager->mask.getX(),
+                    m_kinectManager->mask.getY(),
+                    m_kinectManager->mask.getWidth(),
+                    m_kinectManager->mask.getHeight()
     );
     
     // Unset color and fill for future drawing operations
@@ -96,20 +89,17 @@ void KinectHandWavy::drawPreviewMaskRectangle() {
 
 // Draw a semi-transparent rectangle over each of the three the actuated sections. This should only be called when the shape display is a transFORM.
 void KinectHandWavy::drawPreviewActuatedSections() {
-    // prevents running without kinectManager
-    if (!m_kinectManager->isConnected()) return;
-    
     // Get the width in inches of the the full transform surface (need to cast shape display manager object first).
     float transformWidth = ((TransformIOManager*)m_CustomShapeDisplayManager)->m_Transform_W;
     
     // Get the actuated section dimensions from the CustomShapeDisplayManager
-    float pixelsPerInch = m_kinectManager->m_mask.getWidth() / transformWidth;
+    float pixelsPerInch = m_kinectManager->mask.getWidth() / transformWidth;
     
     std::vector<ofRectangle> sections = m_CustomShapeDisplayManager->createSections(pixelsPerInch);
     
     // Create a frame buffer with the same dimensions as the cropped signal.
     ofFbo fbo;
-    fbo.allocate(m_kinectManager->m_mask.getWidth(), m_kinectManager->m_mask.getHeight(), GL_RGBA); // GL_RGBA for transparency
+    fbo.allocate(m_kinectManager->mask.getWidth(), m_kinectManager->mask.getHeight(), GL_RGBA); // GL_RGBA for transparency
 
     // Begin drawing into the frame buffer
     fbo.begin();
@@ -123,28 +113,30 @@ void KinectHandWavy::drawPreviewActuatedSections() {
 
     // End drawing into the frame buffer
     fbo.end();
-    fbo.draw(m_kinectManager->m_mask);
+    fbo.draw(m_kinectManager->mask);
 }
 
 void KinectHandWavy::updateHeights() {
-    // prevents running without kinectManager
-    if (!m_kinectManager->isConnected()) return;
     // Add blur to the depth image.
-    ofxCvGrayscaleImage blurredDepthImg = getBlurredDepthImg();
+    //ofxCvGrayscaleImage blurredDepthImg = getBlurredDepthImg();
+    
+    ofShortPixels pix = m_kinectManager->getDepthPixels();
+    m_kinectManager->crop(pix);
+    m_kinectManager->thresholdInterp(pix, 200*256, 220*256, 0, 255*256);
     
     // Pass the current depth image to the shape display manager to get the actuated pixels.
-    ofPixels livePixels = m_CustomShapeDisplayManager->cropToActiveSurface( blurredDepthImg.getPixels() );
+    ofPixels livePixels = m_CustomShapeDisplayManager->cropToActiveSurface( pix );
     
     // Directly copy all pixels from livePixels to heightsForShapeDisplay.
     heightsForShapeDisplay = livePixels;
 }
 
-ofxCvGrayscaleImage KinectHandWavy::getBlurredDepthImg() {
-    ofxCvGrayscaleImage blurredDepthImg = m_kinectManager->croppedDepthImg;
-    blurredDepthImg.blurGaussian(41);
-    
-    return blurredDepthImg;
-}
+//ofxCvGrayscaleImage KinectHandWavy::getBlurredDepthImg() {
+//    ofxCvGrayscaleImage blurredDepthImg = m_kinectManager->croppedDepthImg;
+//    blurredDepthImg.blurGaussian(41);
+//
+//    return blurredDepthImg;
+//}
 
 void KinectHandWavy::keyPressed(int Key) {
     
