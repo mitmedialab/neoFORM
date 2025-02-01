6bd87f2ea8b66c0987b18b1a0ff622ed25a78185
charlcoal <charley.reischer@gmail.com>
2025-02-01 16:53:53 -0500

    added settings for window positions

--- src/main.cpp
+++ src/main.cpp
@@ -2,51 +2,85 @@
 #include "DisplayApp.hpp"
 #include "ofMain.h"
 #include "ofApp.h"
+#include "ofxXmlSettings.h"
 #include "AppManager.hpp"
 #include <memory>
 
 //========================================================================
 int main( ){
+	ofxXmlSettings layoutSettings;
+	layoutSettings.load("layoutSettings.xml");
+
+	bool hasPublicWindow = layoutSettings.getValue("hasPublicWindow", false);
+	bool hasProjectorWindow = layoutSettings.getValue("hasProjectorWindow", false);
+
     // create main window with specific location
-	  ofGLFWWindowSettings settings;
-	  settings.setSize(1010, 1060);
-	  settings.setPosition(glm::vec2(0,0));
-	  settings.resizable = true;
+	ofGLFWWindowSettings settings;
+	settings.setSize(
+		layoutSettings.getValue("mainWindowWidth", 1010), 
+		layoutSettings.getValue("mainWindowHeight", 1060)
+	);
+	settings.setPosition(glm::vec2(
+		layoutSettings.getValue("mainWindowX", 0),
+		layoutSettings.getValue("mainWindowY", 0)
+	));
+	settings.resizable = true;
     settings.title = "control window";
     auto mainWindow = ofCreateWindow(settings);
 
     // create display window
-	  settings.setSize(600, 600);
-	  settings.setPosition(glm::vec2(1020,0));
-	  settings.resizable = true;
-    settings.title = "public display window";
-	  // uncomment next line to share main's OpenGL resources with gui
-	  //settings.shareContextWith = mainWindow;
-	  auto displayWindow = ofCreateWindow(settings);
-	  //settingsWindow->setVerticalSync(false);
+	std::shared_ptr<ofAppBaseWindow> displayWindow;
+	if (hasPublicWindow) {
+		settings.setSize(
+			layoutSettings.getValue("publicWindowWidth", 600), 
+			layoutSettings.getValue("publicWindowHeight", 600)
+		);
+		settings.setPosition(glm::vec2(
+			layoutSettings.getValue("publicWindowX", 1020),
+			layoutSettings.getValue("publicWindowY", 0)
+		));
+		settings.resizable = true;
+    	settings.title = "public display window";
+		// uncomment next line to share main's OpenGL resources with gui
+		//settings.shareContextWith = mainWindow;
+		displayWindow = ofCreateWindow(settings);
+	} else {
+		displayWindow = nullptr;
+	}
     
     // create display window
-	  settings.setSize(600, 600);
-	  settings.setPosition(glm::vec2(1020,600));
-	  settings.resizable = true;
-    settings.title = "projector window";
-	  auto projectorWindow = ofCreateWindow(settings);
+	std::shared_ptr<ofAppBaseWindow> projectorWindow;
+	if (hasProjectorWindow) {
+		settings.setSize(
+			layoutSettings.getValue("projectorWindowWidth", 600), 
+			layoutSettings.getValue("projectorWindowHeight", 600)
+		);
+		settings.setPosition(glm::vec2(
+			layoutSettings.getValue("projectorWindowX", 1020),
+			layoutSettings.getValue("projectorWindowY", 600)
+		));
+		settings.resizable = true;
+    	settings.title = "projector window";
+		projectorWindow = ofCreateWindow(settings);
+	} else {
+		projectorWindow = nullptr;
+	}
     
     // seperate applications (needed for some openframeworks stuff)
     auto manager = make_shared<AppManager>();
-    auto displayApp = make_shared<DisplayApp>(600, 600);
-    auto projectorApp = make_shared<ProjectorApp>(600, 600);
+    auto displayApp = hasPublicWindow ? make_shared<DisplayApp>(displayWindow->getWidth(), displayWindow->getHeight()) : nullptr;
+    auto projectorApp = hasProjectorWindow ? make_shared<ProjectorApp>(projectorWindow->getWidth(), projectorWindow->getHeight()) : nullptr;
 
     // let the windows see each other
-    displayApp->mainApp = manager;
+    if (hasPublicWindow) displayApp->mainApp = manager;
     manager->displayWindow = displayWindow;
-    projectorApp->mainApp = manager;
+    if (hasProjectorWindow) projectorApp->mainApp = manager;
     manager->projectorWindow = projectorWindow;
     manager->cam = &displayApp->cam;
 
     ofRunApp(mainWindow, manager);
-    ofRunApp(displayWindow, displayApp);
-    ofRunApp(projectorWindow, projectorApp);
-	  ofRunMainLoop();
+    if (hasPublicWindow) ofRunApp(displayWindow, displayApp);
+    if (hasProjectorWindow) ofRunApp(projectorWindow, projectorApp);
+	ofRunMainLoop();
 
 }
